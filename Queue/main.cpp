be38813086f57ba065c933e387d206acd14bb66f
BIswash Adhikari <biswashadhikari@BIswashs-MacBook-Air.local>
2017-10-26 14:33:12 -0400

    Initial Commit

---
+++ Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by BIswash Adhikari on 10/26/17.
+//  Copyright © 2017 BIswash Adhikari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
